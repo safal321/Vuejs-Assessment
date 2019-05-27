01455c9c334b48d2e8466e03d8e992c78c4b07c7
Safal Sigdel <safalsigdel@gmail.com>
2019-05-27 23:48:03 +0545

    formatting

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,33 +6,40 @@
         <div class="row">
             <div class="col-sm-6">
                     <div class="form-group">
+
                         <label for="name">Name</label>
                         <input id="name"  v-validate="'required|alpha_spaces'" type="text" v-model="name" name="name" class="form-control" placeholder="Enter name" data-vv-as="Name">
                         <span class="red">{{errors.first('name')}}</span>
+
                     </div>
                     <div class="form-group">
                         <label for="gender">Gender</label>
                         <select v-model="gender"  name="gender" id="gender" class="form-control" v-validate="'required'" data-vv-as="Gender">
+
                             <option disabled value="">Select gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
 
                         </select>
                         <span class="red">{{errors.first('gender')}}</span>
-
-
                     </div>
+
                     <div class="form-group">
+
                         <label for="phone">Phone</label>
                         <input id="phone" type="text" v-model="phone" name="phone" class="form-control" placeholder="Enter phone" v-validate="'required|numeric|digits:10'" data-vv-as="Phone">
                         <span class="red">{{errors.first('phone')}}</span>
+
                     </div>
                     <div class="form-group">
+
                         <label for="email">Email</label>
                         <input v-validate="'required|email'" id="email" name="email" type="text" v-model="email" class="form-control" placeholder="Enter email" data-vv-as="Email">
                         <span class="red">{{ errors.first('email') }}</span>
+
                     </div>
                 <div class="form-group country">
+
                     <label for="country">Country</label>
                     <input id="country" type="text" name="country" v-model="country" class="form-control" placeholder="Enter country" v-validate="'required|alpha'" data-vv-as="Country">
                     <span class="red">{{errors.first('country')}}</span>
@@ -41,26 +48,35 @@
             <div class="col-sm-6">
 
                 <div class="form-group">
+
                     <label for="address">Address</label>
                     <input id="address" type="text" name="address" v-model="address" class="form-control" placeholder="Enter address" v-validate="'required'" data-vv-as="Address">
                     <span class="red">{{errors.first('address')}}</span>
+
                 </div>
                 <div class="form-group">
+
                     <label for="nationality">Nationality</label>
                     <input id="nationality" v-model="nationality" type="text" name="nationality" class="form-control" placeholder="Enter nationality" v-validate="'required|alpha_spaces'" data-vv-as="Nationality">
                     <span class="red">{{errors.first('nationality')}}</span>
+
                 </div>
                 <div class="form-group">
+
                     <label for="dateOfBirth">Date of birth</label>
                     <datepicker v-validate="'required'"  id="dateOfBirth" name="dateOfBirth" class="form-control"  placeholder="Select date" v-model="dateOfBirth" data-vv-as="Date of birth"></datepicker>
                     <span class="red">{{errors.first('dateOfBirth')}}</span>
+
                 </div>
                 <div class="form-group">
+
                     <label for="educationBackground">Education background</label>
                     <textarea rows="1" cols="1" id="educationBackground" name="educationBackground" v-model="educationBackground" type="text" class="form-control" placeholder="Enter education background" v-validate="'required'" data-vv-as="Education background"></textarea>
                     <span class="red">{{errors.first('educationBackground')}}</span>
+
                 </div>
                 <div class="form-group">
+
                     <label for="preferredModeOfContact">Preferred mode of contact</label>
                     <select name="preferredModeOfContact" v-model="preferredModeOfContact" id="preferredModeOfContact" class="form-control" v-validate="'required'" data-vv-as="Preferred mode of contact">
                         <option disabled value="">Select contact mode</option>
@@ -69,6 +85,7 @@
                         <option value="none">None</option>
                     </select>
                     <span  class="red">{{errors.first('preferredModeOfContact')}}</span>
+
                 </div>
             </div>
         </div>
